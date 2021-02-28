ec230f105f5eeb7889ee1732ca3910f4007a50e5
BEN <benjamin.hill28@gmail.com>
2021-02-28 11:25:22 -0800

    added passing tests

--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,30 @@
-from django.test import TestCase
+from django.test import SimpleTestCase, client
+from django.http import response
+from django.urls import reverse
 
-# Create your tests here.
+
+class SnacksTest(SimpleTestCase):
+    def test_home_page_status(self):
+        url = reverse("home_index")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_about_page_status(self):
+        url = reverse("about")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_template(self):
+        url = reverse("home_index")
+        response = self.client.get(url)
+        self.assertTemplateUsed(response, "home_index.html")
+
+    def test_about_template(self):
+        url = reverse("about")
+        response = self.client.get(url)
+        self.assertTemplateUsed(response, "about.html")
+
+    def test_base_template(self):
+        url = reverse("home_index")
+        response = self.client.get(url)
+        self.assertTemplateUsed(response, "base.html")

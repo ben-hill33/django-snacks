9b424c7b445af998d3b9317b35ffbf66a9277146
BEN <benjamin.hill28@gmail.com>
2021-02-28 11:03:12 -0800

    added python-dotenv dependency, stored secret key in dotenv file

--- snacks_project/settings.py
+++ snacks_project/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 
+import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "=-1)5wau-=^)wm8zl^ndqu!fa7(k_8)sr&a$r2%h0^uo@px0a4"
+SECRET_KEY = str(os.getenv("SECRET_KEY"))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
